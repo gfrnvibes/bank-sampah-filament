ba8f7d80e57c3b90a24646b95213d986fb3ac872
Gufron <gupron.nurjalil14@gmail.com>
2025-11-25 00:57:15 +0700

    add any stats

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\WasteSale;
+use App\Models\WasteDeposit;
+use App\Models\BalanceWithdrawal;
+use App\Models\TransactionHistory;
 use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -12,21 +16,34 @@ class StatsOverview extends StatsOverviewWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Saldo Bank', '192.1k')
+            // Waste Sale Total Income - BalanceWithdrawal Total Amount
+            Stat::make('Saldo Bank Sampah', 'Rp ' . number_format(WasteSale::sum('total_income') - BalanceWithdrawal::sum('amount'), 0, '.', '.'))
                 ->description('32k increase')
                 ->color('success')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->descriptionIcon('heroicon-m-arrow-trending-up', IconPosition::Before),
-            Stat::make('Income', '21%')
-                ->description('43k increase')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
-                ->color('primary')
-                ->descriptionIcon('heroicon-m-arrow-trending-up', IconPosition::Before),
-            Stat::make('Outcome', '3:12')
+            
+            // Outcome dari transaction history
+            Stat::make('Pengeluaran', 'Rp ' . number_format(BalanceWithdrawal::sum('amount'), 0, '.', '.'))
                 ->description('12k decrease')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('danger')
                 ->descriptionIcon('heroicon-m-arrow-trending-down', IconPosition::Before),
+
+            
+            Stat::make('Total Transaksi', number_format(TransactionHistory::count(), 0, '.', '.'))
+                ->description('12k transaksi')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('info')
+                ->descriptionIcon('heroicon-m-arrow-trending-up', IconPosition::Before),
+
+            // Total Berat Sampah Terjual, pakai suffix kg
+            Stat::make('Berat Sampah Terjual', number_format(WasteSale::sum('total_weight'), 0, '.', '.').' Kg')
+                ->color('success'),
+
+            // Total Berat Sampah Terkumpul
+            Stat::make('Berat Sampah Terkumpul', number_format(WasteDeposit::sum('total_weight'), 0, '.', '.').' Kg')
+                ->color('danger'),
         ];
     }
 }
